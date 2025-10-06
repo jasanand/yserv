2e9d15a49d215ce4861c271098ba6ff5d01f26c0
Jasmeet Singh Anand <5116263+jasanand@users.noreply.github.com>
2025-10-06 18:50:32 +0100

    added multiprocess load test

--- benchmark.py
+++ benchmark.py
@@ -1,5 +1,7 @@
 import google_benchmark as benchmark
 import requests
+from multiprocessing import Pool
+
 YSERV_URL = 'http://127.0.0.1:8000'
 
 @benchmark.register(name='get_returns_by_ticker_start_end_date')
@@ -10,21 +12,36 @@ def benchmark_get_returns_by_ticker(state):
     while state:
         response = requests.get(f'{YSERV_URL}/returns/AAPL/20230101/20251003')
 
+def _get_returns_by_tickers(_):
+    response = requests.get(f'{YSERV_URL}/returns/NVDA,MSFT,AAPL,GOOG,AMZN,META,AVGO,TSLA,JPM,WMT,ORCL,LLY,V,MA,NFLX,XOM,JNJ,ABBV,PLTR,COST,HD,BAC,PG/20230101/20251003')
+
 @benchmark.register(name='get_returns_by_tickers_start_end_date')
 @benchmark.option.iterations(1)
 @benchmark.option.repetitions(10)
 @benchmark.option.unit(benchmark.kMillisecond)
 def benchmark_get_returns_by_ticker(state):
     while state:
-        response = requests.get(f'{YSERV_URL}/returns/NVDA,MSFT,AAPL,GOOG,AMZN,META,AVGO,TSLA,JPM,WMT,ORCL,LLY,V,MA,NFLX,XOM,JNJ,ABBV,PLTR,COST,HD,BAC,PG/20230101/20251003')
+        _get_returns_by_tickers(None)
+
+def _get_returns_by_date():
+    response = requests.get(f'{YSERV_URL}/returns/20250811/NVDA,MSFT,AAPL,GOOG,AMZN,META,AVGO,TSLA,JPM,WMT,ORCL,LLY,V,MA,NFLX,XOM,JNJ,ABBV,PLTR,COST,HD,BAC,PG')
 
 @benchmark.register(name='get_returns_by_tickers_query_date')
 @benchmark.option.iterations(1)
 @benchmark.option.repetitions(10)
 @benchmark.option.unit(benchmark.kMillisecond)
-def benchmark_get_xs_returns_by_date(state):
+def benchmark_get_returns_by_date(state):
     while state:
-        response = requests.get(f'{YSERV_URL}/returns/20250811/NVDA,MSFT,AAPL,GOOG,AMZN,META,AVGO,TSLA,JPM,WMT,ORCL,LLY,V,MA,NFLX,XOM,JNJ,ABBV,PLTR,COST,HD,BAC,PG')
+        _get_returns_by_date()
+
+@benchmark.register(name='get_returns_by_tickers_start_end_date_load_test')
+@benchmark.option.iterations(1)
+@benchmark.option.repetitions(10)
+@benchmark.option.unit(benchmark.kMillisecond)
+def benchmark_get_returns_by_tickers_load_test(state):
+    while state:
+        with Pool(processes=8) as pool:
+            pool.map(_get_returns_by_tickers,range(8))
 
 if __name__ == '__main__':
     benchmark.main()
